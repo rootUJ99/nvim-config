53eb9a81bebb279e0f2d9d862623ca99c4e5c33a
UJwal <ujwal99arak@gmail.com>
2022-10-05 16:19:31 +0530

    initial commit for basic nvim config

---
+++ lua/rootuj/init.lua
@@ -0,0 +1,1 @@
+require("rootuj.set")
---
+++ lua/rootuj/set.lua
@@ -0,0 +1,10 @@
+vim.opt.guicursor = ""
+
+vim.opt.relativenumber = true
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.g.mapleader = " "
